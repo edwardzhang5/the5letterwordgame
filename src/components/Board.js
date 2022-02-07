f19275e34afcd7488b52c0129098e4825f72a414
Taylor Frederick <taylofrederick@gmail.com>
2022-02-07 00:21:21 -0500

    Minor Updates just for Funsies

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useRef , useEffect} from 'react'
-import './Board.css'
 
 
 function Board(props) {
@@ -27,23 +26,23 @@ function Board(props) {
     return (
         <div className="container">
             <div className="left-col">
-            <span>List</span>
-                <ul className='flex-container'>
-                    <div className = 'words'>
-                    {props.Players[0][2].map((item, i) => (<li key={`item_${i}`}>{item}</li>))}
-                    </div>
-                    <div className = 'numbers'>
-                    {props.Players[0][3].map((item, i) => (<li key={`item_${i}`}>{item}</li>))}
-                    </div>
-                </ul>
+                <span>{props.Players[0][0]}</span>
+                    <ul className='words-list'>
+                        <div className = 'words'>
+                        {props.Players[0][2].map((item, i) => (<li key={`item_${i}`}>{item}</li>))}
+                        </div>
+                        <div className = 'numbers'>
+                        {props.Players[0][3].map((item, i) => (<li key={`item_${i}`}>{item}</li>))}
+                        </div>
+                    </ul>
                 <div ref={bottomRefLeft} className='list-bottom'>
 
                 </div>
             </div>
 
             <div className="right-col">
-                <span>List</span>
-                <ul className='flex-container'>
+                <span>{props.Players[0][0]}</span>
+                <ul className='words-list'>
                     <div className = 'words'>
                     {props.Players[1][2].map((item, i) => (<li key={`item_${i}`}>{item}</li>))}
                     </div>
