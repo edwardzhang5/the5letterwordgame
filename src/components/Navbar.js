a06ea9108698d90a93c0e604504012f74b50460b
edwardzhang5 <edwardzhang5@gmail.com>
2022-02-15 23:51:05 -0500

    navbar changes :D

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,36 +2,12 @@ import { useEffect } from 'react'
 import React from 'react'
 import nav from 'react-bootstrap'
 
-// https://jfelix.info/blog/create-a-mobile-friendly-navigation-with-react
-
-// const Navbar = (props) => {
-//   return (
-//     <div classNameName = "flex-container-header">
-//       <h1>5-Letter Word Game</h1>
-//       <div classNameName = "flex-container-helper">
-//         <div classNameName="dropdown">
-//           <button classNameName="drop-button">
-//             <p>Home</p>
-//           </button>
-//         </div>
-//         <div classNameName="dropdown">
-//           <button classNameName="drop-button">
-//             <p>Settings</p>
-//           </button>
-//         </div>
-//         <div classNameName="dropdown">
-//           <button classNameName="drop-button" >
-//             <p>Help</p>
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
 const Navbar = () => (
   <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
     <div className='container-md'>
+      <a className='navbar-brand' href='#'>
+        <img src='logo192.png' width='50' height='50' alt='logo' />
+      </a>
       <a className='navbar-brand' href='#'>
         The 5-Letter Word Game
       </a>
@@ -47,7 +23,7 @@ const Navbar = () => (
         <span className='navbar-toggler-icon'></span>
       </button>
       <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-        <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+        <ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
           <li className='nav-item'>
             <a
               className='nav-link'
