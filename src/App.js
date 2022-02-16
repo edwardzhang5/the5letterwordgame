0a4f91b481b917565aadacd8a6de65182db2fdf3
edwardzhang5 <edwardzhang5@gmail.com>
2022-02-15 23:21:15 -0500

    format

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState } from 'react'
 import Board from './components/Board'
 import WinPage from './components/WinPage'
 import InfoPage from './components/InfoPage'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 //Reset button
 
 function App() {
@@ -26,64 +26,82 @@ function App() {
 
   const [infoTrigger, setInfoTrigger] = useState(false)
   const [win, setWin] = useState(0)
-  
-  
+
   return (
     <div className='wrapper'>
-      <Navbar 
-      setInfoTrigger = {setInfoTrigger}
-      />
-      <div className = "container-sm">
-      <div style={{textAlign:'center'}}>
-       <h1> Player {currPlayer}'s Turn</h1>
+      <Navbar setInfoTrigger={setInfoTrigger} />
+      <div className='container-sm'>
+        <div style={{ textAlign: 'center' }}>
+          <h1> Player {currPlayer}'s Turn</h1>
+        </div>
+        <Board
+          Players={[
+            [player1Name, player1Word, player1WordList, player1Numbers],
+            [player2Name, player2Word, player2WordList, player2Numbers],
+          ]}
+          setPlayer1WordList={setPlayer1WordList}
+          setPlayer2WordList={setPlayer2WordList}
+          setPlayer1Numbers={setPlayer1Numbers}
+          setPlayer2Numbers={setPlayer2Numbers}
+        />
+        <Keyboard
+          Players={[
+            [player1Name, player1Word, player1WordList, player1Numbers],
+            [player2Name, player2Word, player2WordList, player2Numbers],
+          ]}
+          setPlayers={[
+            [
+              setPlayer1Name,
+              setPlayer1Word,
+              setPlayer1WordList,
+              setPlayer1Numbers,
+            ],
+            [
+              setPlayer2Name,
+              setPlayer2Word,
+              setPlayer2WordList,
+              setPlayer2Numbers,
+            ],
+          ]}
+          setPlayer1Name={setPlayer1Name}
+          setPlayer1Word={setPlayer1Word}
+          setPlayer1WordList={setPlayer1WordList}
+          setPlayer2Name={setPlayer2Name}
+          setPlayer2Word={setPlayer2Word}
+          setPlayer2WordList={setPlayer2WordList}
+          setPlayer1Numbers={setPlayer1Numbers}
+          setPlayer2Numbers={setPlayer2Numbers}
+          win={win}
+          setWin={setWin}
+          currPlayer={currPlayer}
+          setPlayer={setPlayer}
+        />
+        <StartPage
+          trigger={buttonStart}
+          setTrigger={setStart}
+          currPlayer={currPlayer}
+          Players={[
+            [player1Name, player1Word, player1WordList, player1Numbers],
+            [player2Name, player2Word, player2WordList, player2Numbers],
+          ]}
+          setPlayer1Name={setPlayer1Name}
+          setPlayer1Word={setPlayer1Word}
+          setPlayer1WordList={setPlayer1WordList}
+          setPlayer2Name={setPlayer2Name}
+          setPlayer2Word={setPlayer2Word}
+          setPlayer2WordList={setPlayer2WordList}
+        />
+        <WinPage
+          Players={[
+            [player1Name, player1Word, player1WordList, player1Numbers],
+            [player2Name, player2Word, player2WordList, player2Numbers],
+          ]}
+          win={win}
+        />
+        <InfoPage infoTrigger={infoTrigger} setInfoTrigger={setInfoTrigger} />
       </div>
-      <Board
-        Players={[[player1Name, player1Word, player1WordList, player1Numbers], [player2Name, player2Word, player2WordList, player2Numbers]]}
-        setPlayer1WordList={setPlayer1WordList}
-        setPlayer2WordList={setPlayer2WordList}
-        setPlayer1Numbers={setPlayer1Numbers}
-        setPlayer2Numbers={setPlayer2Numbers}
-      />
-      <Keyboard
-        Players={[[player1Name, player1Word, player1WordList, player1Numbers], [player2Name, player2Word, player2WordList, player2Numbers]]}
-        setPlayers={[[setPlayer1Name, setPlayer1Word, setPlayer1WordList, setPlayer1Numbers], [setPlayer2Name, setPlayer2Word, setPlayer2WordList, setPlayer2Numbers]]}
-        setPlayer1Name={setPlayer1Name}
-        setPlayer1Word={setPlayer1Word}
-        setPlayer1WordList={setPlayer1WordList}
-        setPlayer2Name={setPlayer2Name}
-        setPlayer2Word={setPlayer2Word}
-        setPlayer2WordList={setPlayer2WordList}
-        setPlayer1Numbers={setPlayer1Numbers}
-        setPlayer2Numbers={setPlayer2Numbers}
-        win={win}
-        setWin={setWin}
-        currPlayer={currPlayer}
-        setPlayer={setPlayer}
-      />
-      <StartPage
-        trigger={buttonStart}
-        setTrigger={setStart}
-        currPlayer={currPlayer}
-        Players={[[player1Name, player1Word, player1WordList, player1Numbers], [player2Name, player2Word, player2WordList, player2Numbers]]}
-        setPlayer1Name={setPlayer1Name}
-        setPlayer1Word={setPlayer1Word}
-        setPlayer1WordList={setPlayer1WordList}
-        setPlayer2Name={setPlayer2Name}
-        setPlayer2Word={setPlayer2Word}
-        setPlayer2WordList={setPlayer2WordList}
-      />
-      <WinPage
-        Players={[[player1Name, player1Word, player1WordList, player1Numbers], [player2Name, player2Word, player2WordList, player2Numbers]]}
-        win={win}
-      />
-      <InfoPage
-        infoTrigger = {infoTrigger}
-        setInfoTrigger = {setInfoTrigger}
-      />
-    </div>
     </div>
   )
 }
-
 
 export default App
